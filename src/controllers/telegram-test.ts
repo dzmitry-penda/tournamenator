8744a1f669ddb366a4588cd0af451efddb99b846
dzmitry-penda <dzmitry.penda@itechart-group.com>
2019-04-04 17:41:54 +0300

    Add a bit of logging

--- src/controllers/telegram-test.ts
+++ src/controllers/telegram-test.ts
@@ -1,16 +1,15 @@
-import client from "../client";
+import client from '../client';
 
 export const getTest = (req, res, next) => {
   res.json({ message: 'hello world'});
 };
 
 export const postTest = async(req, res, next) => {
+  console.log('sending1', req)
   client
     .sendMessage(req.body.message.chat.id, 'I am GROOT?')
     .promise()
     .then(function(){
-      client
-        .sendMessage(req.body.message.chat.id, 'I am GROOT!')
       res.json({ ok: true });
     })
     .catch(next);
