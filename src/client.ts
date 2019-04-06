2cc4f3e98e9a3d36f2b8bcc02edf3974df0a2b7d
dzmitry-penda <dzmitry.penda@itechart-group.com>
2019-04-06 19:11:40 +0300

    Remove client on

--- src/client.ts
+++ src/client.ts
@@ -2,14 +2,6 @@ import * as TelegramBotClient from 'telegram-bot-client';
 const client = new TelegramBotClient(process.env.TELEGRAM_API_TOKEN);
 
 
-client.on('inline.callback.query', (msg) => {
-
-  var data = msg; //Value from 'callback_data' field of clicked button
-
-  console.log(data);
-
-  //do stuff
-});
 
 
 export default client;
