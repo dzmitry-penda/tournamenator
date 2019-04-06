b38a96f27c779bee23d1ba1ace9f9d11bc24f0f1
dzmitry-penda <dzmitry.penda@itechart-group.com>
2019-04-06 15:50:37 +0300

    add log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import testRouter from './test';
 import telegramRouter from './telegram-test';
 
 const router = Router();
-
+console.log(`/${process.env.WEBHOOK_TOKEN}`);
 router.use('/test', testRouter);
 router.use(`/${process.env.WEBHOOK_TOKEN}`, telegramRouter);
 
