aa391023c369d22da494c1dcb5546b5ee94e5bab
dzmitry-penda <dzmitry.penda@itechart-group.com>
2019-04-06 21:12:24 +0300

    Log

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import * as TelegramBotClient from 'telegram-bot-client';
 const client = new TelegramBotClient(process.env.TELEGRAM_API_TOKEN);
-
+console.log(client)
 client.on('inline.query', function(message)
 {
 	// Received inline query
