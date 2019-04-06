6a2f4d1f8f8c7bdceb8c3fa41a47933f330f8234
dzmitry-penda <dzmitry.penda@itechart-group.com>
2019-04-06 21:24:02 +0300

    Remove unneeded stuff

--- src/client.ts
+++ src/client.ts
@@ -1,23 +1,6 @@
 import * as TelegramBotClient from 'telegram-bot-client';
 const client = new TelegramBotClient(process.env.TELEGRAM_API_TOKEN);
-console.log(client)
-client.on('inline.query', function(message)
-{
-	// Received inline query
-    console.log('iq',message);
-});
 
-client.on('inline.result', function(message)
-{
-	// Received chosen inline result
-    console.log('ir',message);
-});
-
-client.on('inline.callback.query', function(message)
-{
-	// New incoming callback query
-    console.log('icq',message);
-});
 
 
 
